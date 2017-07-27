5e0f51f578418eafda03666157ab5be0d1d37473
ywx469371 <yutingcong@huawei.com>
2017-07-27 16:21:01 +0800

    intent update

--- index.js
+++ index.js
@@ -26,17 +26,52 @@ var bot = new builder.UniversalBot(connector, function (session) {
 var recognizer = new builder.LuisRecognizer(process.env.LUIS_MODEL_URL);
 bot.recognizer(recognizer);
 
-bot.dialog('remind', function (session, args) {
+bot.dialog('Business_Remind', function (session, args) {
     // retrieve hotel name from matched entities
-    var hotelEntity = builder.EntityRecognizer.findEntity(args.intent.entities, 'datetimeV2');
-      session.endDialog('小威已经给你预定提醒！时间为：' + JSON.stringify(args.intent.entities )+hotelEntity);
+    var datetimeV2 = builder.EntityRecognizer.findEntity(args.intent.entities, 'datetimeV2');
+    var timeStr = args.intent.entities[0].resolution.values.length >1? args.intent.entities[0].resolution.values[1].value :
+    args.intent.entities[0].entity;
+      session.endDialog('小薇已经给你预定提醒！时间为：' + timeStr);
 }).triggerAction({
-    matches: 'remind'
+    matches: 'Business_Remind'
+});
+
+bot.dialog('Business_Remind_Times', function (session, args) {
+    // retrieve hotel name from matched entities
+    var datetimeV2 = builder.EntityRecognizer.findEntity(args.intent.entities, 'datetimeV2');
+      var timeStr = args.intent.entities[0].resolution.values.length >1? args.intent.entities[0].resolution.values[1].value :
+    args.intent.entities[0].entity;
+      session.endDialog('小薇已经给你预定提醒！时间为：' + timeStr);
+}).triggerAction({
+    matches: 'Business_Remind_Times'
 });
  
+
+bot.dialog('FAQ_Help', function (session) {
+   retrieveKB(session,'你会啥') ;
+}).triggerAction({
+    matches: 'FAQ_Help'
+});
+
+bot.dialog('FAQ_Hi', function (session) {
+   retrieveKB(session,'hi') ;
+}).triggerAction({
+    matches: 'FAQ_Hi'
+});
+
+bot.dialog('FAQ_Introduce', function (session) {
+   retrieveKB(session,'你是谁') ;
+}).triggerAction({
+    matches: 'FAQ_Introduce'
+});
+
+bot.dialog('FAQ_Leave', function (session) {
+   session.endDialog('代请假功能即将开放！');
+}).triggerAction({
+    matches: 'FAQ_Leave'
+});
+
 bot.dialog('None', function (session, args) {
-    // retrieve hotel name from matched entities
-    //var hotelEntity = builder.EntityRecognizer.findEntity(args.intent.entities, 'Hotel');
 
      retrieveKB(session) ;
       // session.endDialog('干嘛！无聊！');
@@ -44,24 +79,17 @@ bot.dialog('None', function (session, args) {
     matches: 'None'
 });
 
-bot.dialog('help', function (session) {
- 
-   retrieveKB(session) ;
-   
 
-    //ession.endDialog('我暂时只提供定时提醒服务！');
-}).triggerAction({
-    matches: 'help'
-});
+
 
 
   axios.defaults.baseURL = 'https://westus.api.cognitive.microsoft.com/qnamaker/v2.0/knowledgebases/f1f1bb67-75a4-4829-b0ad-53416969e978/generateAnswer';
   axios.defaults.headers.common['Ocp-Apim-Subscription-Key'] = '970f8ee823244168acf07ea3938afcd9';
   axios.defaults.headers.post['Content-Type'] = 'application/json';
  
- function retrieveKB(session){
+ function retrieveKB(session,message){
  
-    axios.post( axios.defaults.baseURL ,{"question": session.message.text})
+    axios.post( axios.defaults.baseURL ,{"question": message||session.message.text})
     .then(function (response) {
     console.log(JSON.stringify(response.data));
      session.endDialog(response.data.answers[0].answer) ;
